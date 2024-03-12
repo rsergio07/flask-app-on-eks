5336d829d34b879aac3ec8044bf6bbc13c16306e
Sergio Romero <111907025+rsergio07@users.noreply.github.com>
2024-03-12 17:54:02 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -90,12 +90,31 @@ resource "aws_eks_cluster" "my_cluster" {
   role_arn = aws_iam_role.eks_cluster_role.arn
 
   vpc_config {
-    subnet_ids         = aws_subnet.eks_subnets[*].id
-    security_group_ids = [aws_security_group.eks_sg.id]
+    subnet_ids              = aws_subnet.eks_subnets[*].id
+    security_group_ids      = [aws_security_group.eks_sg.id]
+    endpoint_private_access = false
+    endpoint_public_access  = true
   }
 }
 
 # Create ECR Repository
 resource "aws_ecr_repository" "my_ecr_repo" {
   name = var.ecr_repository_name
+}
+
+# Create EKS Node Group
+resource "aws_eks_node_group" "my_cluster_nodes" {
+  cluster_name    = aws_eks_cluster.my_cluster.name
+  node_group_name = "my-cluster-nodes"
+  node_role_arn   = aws_iam_role.eks_cluster_role.arn
+  subnet_ids      = aws_subnet.eks_subnets[*].id
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
+  }
+
+  # Additional configurations as needed
+  instance_types = ["t2.micro"]
 }
