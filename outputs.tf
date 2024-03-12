2028c289ffe5135f75be8d4cfad3698a53865477
Sergio Romero <111907025+rsergio07@users.noreply.github.com>
2024-03-12 13:19:51 -0600

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,5 @@
-output "cluster_endpoint" {
-    value = aws_eks_cluster.cluster.endpoint
-    description = "Endpoint of the EKS cluster"
-}
-
-output "cluster_name" {
-    value = aws_eks_cluster.cluster.name
-    description = "Name of the EKS cluster"
-}
-
-output "service_url" {
-    value = format("%s:%d", aws_eks_cluster.cluster.endpoint, var.service_port)
-    description = "URL of the deployed Flask app service"
+# Defines an output block to represent the URL of the deployed Flask app
+output "app_url" {
+  value       = format("%s:%d", aws_eks_cluster.my_cluster.endpoint, var.service_port)
+  description = "URL of the deployed Flask app"
 }
