a6997eaa4bb4d893fc81bd387024b72e99ac3c9b
Sergio Romero <111907025+rsergio07@users.noreply.github.com>
2024-03-12 14:04:18 -0600

    Update S3-backend.tf

--- S3-backend.tf
+++ S3-backend.tf
@@ -1,9 +1,9 @@
 # Specifies the backend configuration for storing the Terraform state file in an S3 bucket
 terraform {
   backend "s3" {
-    region  = "us-east-1"
-    profile = "default"
-    key     = "terraformstatefile"
-    bucket  = "rsergio-terraform-statefile-bucket"
+    bucket         = "rsergio-terraform-statefile-bucket"
+    key            = "terraform.tfstate"
+    region         = var.AWS_REGION
+    dynamodb_table = "terraform-lock-table"
   }
 }
