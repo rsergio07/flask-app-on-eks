5e006b5609ae6f88c0e0618d3e93949b46be9730
Sergio Romero <111907025+rsergio07@users.noreply.github.com>
2024-03-12 18:15:11 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -56,14 +56,16 @@ resource "aws_security_group" "eks_sg" {
 }
 
 # Create IAM Role for EKS Cluster
+# Create IAM Role for EKS Cluster
 resource "aws_iam_role" "eks_cluster_role" {
   name = "eks-cluster-role"
+
   assume_role_policy = jsonencode({
     "Version" : "2012-10-17",
     "Statement" : [{
       "Effect" : "Allow",
       "Principal" : {
-        "Service" : "eks.amazonaws.com"
+        "Service" : ["eks.amazonaws.com", "ec2.amazonaws.com"]
       },
       "Action" : "sts:AssumeRole"
     }]
