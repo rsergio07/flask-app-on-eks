365c9686ca5c21806c72f7dbb839a0145385ae67
Sergio Romero <111907025+rsergio07@users.noreply.github.com>
2024-03-13 09:45:20 -0600

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,9 @@
-# Defines an output block to represent the URL of the deployed Flask app
+# Internet Gateway ID
+output "internet_gateway_id" {
+  value = aws_internet_gateway.my_igw.id
+}
+
+# URL of the deployed Flask app
 output "app_url" {
   value       = format("%s:%d", aws_eks_cluster.my_cluster.endpoint, var.service_port)
   description = "URL of the deployed Flask app"
